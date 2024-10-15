d8b7f4d79199264c8a5d1d918123b8588d109150
vincenzo.maniscalco <vincenzo.maniscalco@udc.es>
2024-10-15 15:57:57 +0200

    Primo commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+// se sono in debug faccio un for da 1 a 10 e stampo i
+//se sono in realese faccio il for da 1 a 20 il valore dell indice
+
+Console.ForegroundColor=ConsoleColor.Green;
+
+#region ESERCIZI_15/10/2024
+
+#if DEBUG
+Console.WriteLine("Sono in DEBUG");
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine("Il valore dell indice è "+i);
+}
+#endif
+
+#if RELEASE
+Console.WriteLine("Sono in RELEASE");
+
+for (int i = 1; i <= 20; i++)
+{
+    Console.WriteLine("Il valore dell indice è " + i);
+}
+#endif
+
+#endregion
